dd485134ff8080e4ae124da4213bb11f3b35063b
daveslow3k <daveslow3k@efa9c490-2386-11de-96a1-5542d1fc8aa6>
2009-04-17 14:47:28 +0000

    moved package around

--- Spiral/src/com/reveregroup/client/client/events/PhotoClickHandler.java
+++
@@ -1,7 +0,0 @@
-package com.reveregroup.carousel.client.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface PhotoClickHandler extends EventHandler{
-	public void photoClicked(PhotoClickEvent event);
-}
