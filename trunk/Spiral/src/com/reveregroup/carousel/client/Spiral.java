c0a7a0c8cbc9bb5a96edcb0d4d0325d64ee1dd0b
dwolvert <dwolvert@efa9c490-2386-11de-96a1-5542d1fc8aa6>
2009-04-17 21:21:17 +0000

    added addFocusDecoratorWidget()

--- trunk/Spiral/src/com/reveregroup/carousel/client/Spiral.java
+++ trunk/Spiral/src/com/reveregroup/carousel/client/Spiral.java
@@ -8,13 +8,19 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.reveregroup.carousel.client.events.PhotoFocusEvent;
+import com.reveregroup.carousel.client.events.PhotoFocusHandler;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Spiral implements EntryPoint {
   public Carousel carousel;
+  private Label myLabel = new Label();
+  
   private List<Photo> photos;
 	public void onModuleLoad() {
 	photos = new ArrayList<Photo>(10);
@@ -29,6 +35,15 @@ public class Spiral implements EntryPoint {
 	}
     carousel = new Carousel();
     carousel.setPhotos(photos);
+    
+    carousel.addPhotoFocusHandler(new PhotoFocusHandler() {
+    	public void photoFocused(PhotoFocusEvent event) {
+    		myLabel.setText("Photo " + Integer.toString(event.getPhotoIndex()));
+    	}
+    });
+    
+    carousel.setFocusDecoratorWidget(myLabel, DockPanel.WEST);
+    
     Button clockwise = new Button("prev");
     clockwise.addClickHandler(new ClickHandler(){
 		public void onClick(ClickEvent event) {
