3712f3eb2a719f527af23f73a714f003d46e76fa
daveslow3k <daveslow3k@efa9c490-2386-11de-96a1-5542d1fc8aa6>
2009-04-21 17:15:14 +0000

    Lightbox

--- trunk/Spiral/src/com/reveregroup/carousel/client/FocusBehavior.java
+++ trunk/Spiral/src/com/reveregroup/carousel/client/FocusBehavior.java
@@ -1,10 +1,28 @@
 package com.reveregroup.carousel.client;
 
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.event.shared.HandlerRegistration;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.DockPanel.DockLayoutConstant;
+
 import com.reveregroup.carousel.client.events.PhotoClickEvent;
 import com.reveregroup.carousel.client.events.PhotoClickHandler;
 import com.reveregroup.carousel.client.events.PhotoFocusEvent;
@@ -15,8 +33,13 @@ import com.reveregroup.carousel.client.events.PhotoUnfocusHandler;
 public class FocusBehavior {
 	protected Carousel target;
 	protected HandlerManager handlerManager;
+
+	protected PopupPanel popup;
+	protected Image testPanel;
+
 	protected Widget focusDecoratorWidget = null;
 	
+
 	public FocusBehavior(Carousel carousel) {
 		this.target = carousel;
 		handlerManager = new HandlerManager(this);
@@ -29,6 +52,33 @@ public class FocusBehavior {
 					evt.setPhotoIndex(event.getPhotoIndex());
 					handlerManager.fireEvent(evt);
 				}
+			}
+		});
+		
+		addPhotoFocusHandler(new PhotoFocusHandler() {
+			public void photoFocused(PhotoFocusEvent event) {
+				// TODO Auto-generated method stub
+			    DockPanel tray = new DockPanel();
+			    Element lightbox = (Element)DOM.getElementById("lightbox");		
+				lightbox.getStyle().setProperty("backgroundColor", "black");
+				lightbox.getStyle().setProperty("zIndex", "500");
+				lightbox.getStyle().setProperty("display", "block");
+			    tray.getElement().getStyle().setProperty("zIndex", "150");
+			    popup = new PopupPanel(true,true);
+				Image eventImage = new Image(event.getPhoto().getUrl());
+			    tray.add(eventImage, DockPanel.CENTER);
+			    tray.add(focusDecoratorWidget, DockPanel.SOUTH);			  
+			    popup.getElement().getStyle().setProperty("zIndex", "550");
+			    popup.add(tray);			    
+				popup.center();
+				popup.addCloseHandler(new CloseHandler<PopupPanel>(){
+					public void onClose(CloseEvent<PopupPanel> event) {
+						// TODO Auto-generated method stub
+						Element lightbox = (Element)DOM.getElementById("lightbox");
+						lightbox.getStyle().setProperty("backgroundColor", "white");						
+						lightbox.getStyle().setProperty("zIndex", "-50");
+					}
+				});
 			}
 		});
 	}
@@ -44,4 +94,7 @@ public class FocusBehavior {
 	public void setFocusDecoratorWidget(Widget widget, DockLayoutConstant position) {
 		focusDecoratorWidget = widget;
 	}
+	public static native String getUserAgent() /*-{
+	return navigator.userAgent.toLowerCase();
+	}-*/;
 }
