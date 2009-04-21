3e470df0de260ab0443397571aa0b4847401e83e
dwolvert <dwolvert@efa9c490-2386-11de-96a1-5542d1fc8aa6>
2009-04-21 21:15:55 +0000

    Demo

--- Spiral/src/com/reveregroup/carousel/client/Spiral.java
+++ Spiral/src/com/reveregroup/carousel/client/Spiral.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.EventListener;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
 import com.reveregroup.carousel.client.events.PhotoFocusEvent;
 import com.reveregroup.carousel.client.events.PhotoFocusHandler;
 
@@ -18,49 +22,84 @@ import com.reveregroup.carousel.client.events.PhotoFocusHandler;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Spiral implements EntryPoint {
-  public Carousel carousel;
-  private Label myLabel = new Label();
-  
-  private List<Photo> photos;
+	public Carousel carousel;
+	private Label myLabel = new Label();
+
 	public void onModuleLoad() {
-	photos = new ArrayList<Photo>(10);
-	Photo photo;
-	for(int i = 0; i < 25;i++){
-		photo = new Photo(GWT.getModuleBaseURL()+"sample"+(i+1+".jpg"));
-//	for(int i = 0; i < 10;i++){
-//		photo = new Photo(GWT.getModuleBaseURL()+"img"+(i+".bmp"));
-		photo.setCaption("This is photo caption number: " + i);
-		photo.setPhotoId(i);
-		photos.add(photo);
+		carousel = new Carousel();
+		loadPhotos();
+
+		carousel.addPhotoFocusHandler(new PhotoFocusHandler() {
+			public void photoFocused(PhotoFocusEvent event) {
+				myLabel.setText("Photo " + Integer.toString(event.getPhotoIndex()) + ": " + event.getPhoto().getCaption());
+			}
+		});
+
+		carousel.setFocusDecoratorWidget(myLabel, DockPanel.SOUTH);
+
+		Button clockwise = new Button("prev");
+		clockwise.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				carousel.prev();
+			}
+		});
+		Button counterclockwise = new Button("next");
+		counterclockwise.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				carousel.next();
+			}
+		});
+		RootPanel.get("carouselDiv").add(carousel);
+		RootPanel.get().add(clockwise);
+		RootPanel.get().add(counterclockwise);
+		
+		//Update Photos button
+		Element submitButton = DOM.getElementById("submitPhotoData");
+		Event.setEventListener(submitButton, new EventListener() {
+			public void onBrowserEvent(Event event) {
+				loadPhotos();
+			}
+		});
+		Event.sinkEvents(submitButton, Event.ONCLICK);
+
 	}
-    carousel = new Carousel();
-    carousel.setPhotos(photos);
-    
-    carousel.addPhotoFocusHandler(new PhotoFocusHandler() {
-    	public void photoFocused(PhotoFocusEvent event) {
-    		myLabel.setText("Photo " + Integer.toString(event.getPhotoIndex()));
-    	}
-    });
-    
-    carousel.setFocusDecoratorWidget(myLabel, DockPanel.WEST);
-    
-    Button clockwise = new Button("prev");
-    clockwise.addClickHandler(new ClickHandler(){
-		public void onClick(ClickEvent event) {
-			carousel.prev();
-		}	
-    });
-    Button counterclockwise = new Button("next");
-    counterclockwise.addClickHandler(new ClickHandler(){
-    	public void onClick(ClickEvent event) {
-    		carousel.next();
-    	}	
-    });
-    RootPanel.get("carouselDiv").add(carousel);
-    RootPanel.get().add(clockwise);
-    RootPanel.get().add(counterclockwise);
-    
-    //new Playground(carousel);
-    
-  }
+
+	private void loadPhotos() {
+		String photoString = TextArea.wrap(DOM.getElementById("photoData")).getValue();
+		List<Photo> photos = new ArrayList<Photo>();
+		
+		int lineStart = 0;
+		int lineEnd = photoString.indexOf('\n');
+		int urlEnd;
+		while (lineEnd != -1) {
+			Photo photo = new Photo();
+			urlEnd = photoString.indexOf(' ', lineStart);
+			if (urlEnd == -1 || urlEnd > lineEnd) {
+				urlEnd = lineEnd;
+				photo.setCaption("");
+			} else {
+				photo.setCaption(photoString.substring(urlEnd, lineEnd).trim());
+			}
+			photo.setUrl(photoString.substring(lineStart, urlEnd).trim());
+			if (photo.getUrl().length() > 0)
+				photos.add(photo);
+			lineStart = lineEnd + 1;
+			lineEnd = photoString.indexOf('\n', lineStart);
+		}
+		lineEnd = photoString.length();
+		urlEnd = photoString.indexOf(' ', lineStart);
+		
+		Photo photo = new Photo();
+		if (urlEnd == -1 || urlEnd > lineEnd) {
+			urlEnd = lineEnd;
+			photo.setCaption("");
+		} else {
+			photo.setCaption(photoString.substring(urlEnd, lineEnd).trim());
+		}
+		photo.setUrl(photoString.substring(lineStart, urlEnd).trim());
+		if (photo.getUrl().length() > 0)
+			photos.add(photo);
+		
+		carousel.setPhotos(photos);
+	}
 }
