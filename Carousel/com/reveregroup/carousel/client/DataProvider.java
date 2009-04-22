c943cbfc7d55a24435ef093f9146d26c042b6811
dwolvert <dwolvert@efa9c490-2386-11de-96a1-5542d1fc8aa6>
2009-04-22 14:20:31 +0000

    Cleanup

--- Carousel/com/reveregroup/carousel/client/DataProvider.java
+++
@@ -1,7 +0,0 @@
-package com.reveregroup.carousel.client;
-
-public interface DataProvider<DATATYPE> {
-	public int size();
-	public String getURL(int index);
-	public DATATYPE getData(int index);
-}
