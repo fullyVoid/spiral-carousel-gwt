5a4aa5f02bbbc385e9b241956b08df95aa9447cc
dwolvert <dwolvert@efa9c490-2386-11de-96a1-5542d1fc8aa6>
2009-04-22 14:20:31 +0000

    Cleanup

--- trunk/Carousel/com/reveregroup/carousel/client/Photo.java
+++ trunk/Carousel/com/reveregroup/carousel/client/Photo.java
@@ -3,9 +3,6 @@ package com.reveregroup.carousel.client;
 public class Photo{
 	private String url;
 	private String caption;
-	private int height;
-	private int width;
-	private int photoId;
 	
 	public Photo() {
 	}
@@ -13,11 +10,9 @@ public class Photo{
 	public Photo(String url){
 		this.url = url;
 	}
-	public Photo(String url,String caption, int height,int width){
+	public Photo(String url,String caption){
 		this.url = url;
 		this.caption = caption;
-		this.height = height;
-		this.width = width;
 	}
 	public String getCaption() {
 		return caption;
@@ -25,30 +20,10 @@ public class Photo{
 	public void setCaption(String caption) {
 		this.caption = caption;
 	}
-	public int getHeight() {
-		return height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
-	}
 	public String getUrl() {
 		return url;
 	}
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	public int getWidth() {
-		return width;
-	}
-	public void setWidth(int width) {
-		this.width = width;
-	}
-	public void setPhotoId(int photoId) {
-		this.photoId = photoId;
-	}
-	public int getPhotoId() {
-		return photoId;
-	}
-	
-	
 }
